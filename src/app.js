10ef5759cb553c388e92be15d5be6347c8137317
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2023-11-18 15:18:38 +0000

    Terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function excuseGenerator() {
+  let who = ["The dinosaur", "My dad", "His pig", "My uncle"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let action = ["ate", "peed", "crushed", "broke"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let what = ["my homework", "the phone", "the bicycle"];
+
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my breakfast",
+    "while I was writing"
+  ];
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
+}
+console.log(excuseGenerator());
